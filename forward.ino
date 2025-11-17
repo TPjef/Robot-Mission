b699e384e39dbcdce410e6b1aec2b7f48ddbfb2f
Tammanoon <tammanunpimpak@gmail.com>
2025-11-17 23:08:53 +0700

    Update forward.ino

--- forward.ino
+++ forward.ino
@@ -7,3 +7,23 @@ void forward() {
   digitalWrite(LM1, HIGH);
   digitalWrite(LM2, LOW);
 }
+void left1() {
+  analogWrite(PWM1, speed1);
+  digitalWrite(RM1, HIGH);
+  digitalWrite(RM2, LOW);
+
+  analogWrite(PWM2, speed2);
+  digitalWrite(LM1, LOW);
+  digitalWrite(LM2, HIGH);
+}
+void right1() {
+  analogWrite(PWM1, speed1);
+  digitalWrite(RM1, LOW);
+  digitalWrite(RM2, HIGH);
+
+  analogWrite(PWM2, speed2);
+  digitalWrite(LM1, HIGH);
+  digitalWrite(LM2, LOW);
+
+}
+
