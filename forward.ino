b61be278af86bc35eccabff76bbdf4d00e6bc4a6
Tammanoon <tammanunpimpak@gmail.com>
2025-12-02 15:03:54 +0700

    Implement left2 and right2 functions for movement
    Added left2 and right2 functions for additional movement control based on sensor readings.

--- forward.ino
+++ forward.ino
@@ -27,3 +27,26 @@ void right1() {
 
 }
 
+void left2(){
+  if(digitalRead(BL)==0 and digitalRead(BR)==1)
+  {
+    analogWrite(PWM1,speed1);
+    digitalWrite(RM1,HIGH);
+    digitalWrite(RM2,LOW);
+    analogWrite(PWM2,speed2);
+    digitalWrite(LM1,LOW);
+    digitalWrite(LM2,HIGH);
+  }
+}
+
+void right2(){
+  if(digitalRead(BL)==1 and digitalRead(BR)==0)
+  {
+    analogWrite(PWM1,speed1);
+    digitalWrite(RM1,LOW);
+    digitalWrite(RM2,HIGH);
+    analogWrite(PWM2,speed2);
+    digitalWrite(LM1,HIGH);
+    digitalWrite(LM2,LOW);
+  }
+}
