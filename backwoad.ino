bc799c87d9e5110d90553e7487c8d2ba60f7bcf7
Tammanoon <tammanunpimpak@gmail.com>
2025-12-02 15:03:00 +0700

    Update backwoad.ino

--- backwoad.ino
+++ backwoad.ino
@@ -16,7 +16,7 @@ void right() {
   analogWrite(PWM2, speed2);  
   digitalWrite(LM1, LOW);
   digitalWrite(LM2, HIGH);
-  delay(500);
+  delay(600);
 
   analogWrite(PWM1, speed1);
   digitalWrite(RM1, LOW);    
@@ -25,7 +25,7 @@ void right() {
   analogWrite(PWM2, speed2);
   digitalWrite(LM1, HIGH);  
   digitalWrite(LM2, LOW);
-  delay(370);
+  delay(500);
 }
 
 void left() {
@@ -37,7 +37,7 @@ void left() {
   analogWrite(PWM2, speed2);
   digitalWrite(LM1, LOW);  
   digitalWrite(LM2, HIGH);
-  delay(500);
+  delay(600);
 
   analogWrite(PWM1, speed1);
   digitalWrite(RM1, HIGH);  
@@ -46,7 +46,7 @@ void left() {
   analogWrite(PWM2, speed2);
   digitalWrite(LM1, LOW);   
   digitalWrite(LM2, HIGH);
-  delay(370);
+  delay(500);
 }
 
 void spin() {
@@ -67,35 +67,4 @@ void stopMotor() {
   digitalWrite(RM2, LOW);
   digitalWrite(LM1, LOW);
   digitalWrite(LM2, LOW);
-}
-
-//Sensor หลัง(กันตก)
-
-void rightSlow() {
-  digitalWrite(LM1, HIGH);
-  digitalWrite(LM2, LOW);
-  analogWrite(PWM2, 80);
-
-  digitalWrite(RM1, LOW);
-  digitalWrite(RM2, HIGH);
-  analogWrite(PWM1, 80);
-}
-
-void leftSlow() {
-  digitalWrite(LM1, LOW);
-  digitalWrite(LM2, HIGH);
-  analogWrite(PWM2, 80);
-
-  digitalWrite(RM1, HIGH);
-  digitalWrite(RM2, LOW);
-  analogWrite(PWM1, 80);
-}
-
-void backSlow() {
-  digitalWrite(LM1, LOW);
-  digitalWrite(LM2, HIGH);
-  digitalWrite(RM1, LOW);
-  digitalWrite(RM2, HIGH);
-  analogWrite(PWM1, 90);
-  analogWrite(PWM2, 90);
 }
